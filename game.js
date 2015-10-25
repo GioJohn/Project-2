9f764bb700495a4a6a6137ab6dc92c3eff72e330
GioJohn <giovan345@gmail.com>
2015-10-25 02:11:14 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,77 @@
+.background { background: url("http://31.media.tumblr.com/7de3c1b6aff46c6513fcd0d9c4ccaecf/tumblr_mws3k2qVs21t0aw2ao1_400.gif");
+                 border-spacing: 0; 
+				 background-repeat: no-repeat;
+				 background-attachment: fixed;
+				 background-size: 45%;
+}
+				 
+.lava{ background-image: url("http://vignette3.wikia.nocookie.net/tibia/images/a/aa/Lava.gif/revision/latest?cb=20141116212949&path-prefix=en"); 
+				
+}
+
+.wall{ background-image: url(http://plummerconcrete.com/wp-content/uploads/2015/09/Concrete_LightGray.jpg);  
+}
+
+.door{ background-image: url("http://www.small-icons.com/stock-icons/small-business/32x32/closed-door.gif");
+	width: 32 px;
+	height: 32 px;
+}
+
+.house{background-image: url("http://img.informer.com/icons/png/32/314/314295.png");
+		width: 32;
+		height: 32;
+}
+
+.actor { 
+			position: absolute;  
+}
+		
+.coin{
+	background-image: url("http://vignette4.wikia.nocookie.net/tibia/images/e/e5/Big_Bone.gif/revision/20060804163828?path-prefix=en");
+	width: 32 px;
+	height: 32 px;
+}
+		
+.player { 
+		background-image: url("http://static.tumblr.com/xmnyyry/ycxm94jtv/dog_by_zioru02-d413fdz.gif");
+		width: 32px;
+		height: 32px;
+		-moz-transform: scaleX(-1);
+        -o-transform: scaleX(-1);
+        -webkit-transform: scaleX(-1);
+        transform: scaleX(-1);
+        filter: FlipH;
+        -ms-filter: "FlipH";
+		
+}
+		
+.run .player{
+		background-image: url("http://static.tumblr.com/xmnyyry/ycxm94jtv/dog_by_zioru02-d413fdz-right.gif");
+}	
+	
+.spikes{
+	background-image: url("http://vignette1.wikia.nocookie.net/sonic/images/e/e4/Spikes_CD.png/revision/latest?cb=20130304215954");
+	width: 32px;
+	height: 32px;
+}
+		
+.key{
+	background-image: url("https://www.ebi.ac.uk/thornton-srv/databases/profunc/templates/gif/key1c.gif");
+	width: 32px;
+	height: 32px;
+}
+	
+.game {
+  overflow: hidden;
+  max-width: 600px;
+  max-height: 600px;
+  position: relative;
+}
+
+.lost .player {
+  background: rgb(160, 64, 64);
+}
+.won .player {
+		background-image:("http://yesiamcheap.com/wordpress/wp-content/uploads/2012/12/youwon1.gif");
+		background-position: center;
+}
